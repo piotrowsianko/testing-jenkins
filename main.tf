c29ff5f7e08e261974af1a7c4cbace4ae9de3f5f
piotr.owsianko <piotr.owsianko@globallogic.com>
2022-10-17 13:56:52 +0200

    Main and providers.tf files created

---
+++ main.tf
@@ -0,0 +1,5 @@
+resource local_file sample_res {
+	filename = "sample.txt"
+	content = " That's a sample file text created by terraform"
+}
+
---
+++ providers.tf
@@ -0,0 +1,5 @@
+provider "google" {
+  project     = "My test project owspio"
+  region      = "europe-central2-a"
+}
+
